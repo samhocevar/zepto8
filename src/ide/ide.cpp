b7527c99dbb7c3732fb0a0c7af2929d1d308103f
Sam Hocevar <sam@hocevar.net>
2018-10-02 01:57:36 +0200

    IDE: palette window (just a test).

--- src/ide/ide.cpp
+++ src/ide/ide.cpp
@@ -36,10 +36,20 @@ void ide::TickGame(float seconds)
 {
     WorldEntity::TickGame(seconds);
 
+    ImGui::ShowDemoWindow();
     ImGui::SetNextWindowFocus();
-    ImGui::Begin("window 1");
+    ImGui::Begin("Palette");
     {
-        ImGui::Text("Hello, world!");
+        for (int i = 0; i < 16; i++)
+        {
+            if (i % 4 > 0)
+                ImGui::SameLine();
+            ImGui::PushID(i);
+            ImGui::PushStyleColor(ImGuiCol_Button, (ImVec4)((lol::vec4)z8::palette::get(i) / 255.f));
+            ImGui::Button(lol::format("%2d", i).c_str());
+            ImGui::PopStyleColor(1);
+            ImGui::PopID();
+        }
     }
     ImGui::End();
     ImGui::Begin("Let's go on an adventure");
