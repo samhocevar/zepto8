097593fca31573b41eca76578ed2dcec593e99f0
Sam Hocevar <sam@hocevar.net>
2020-05-05 13:52:28 +0200

    pico8: split old/new compression and decompression code.

--- src/pico8/code.cpp
+++ src/pico8/code.cpp
@@ -20,6 +20,7 @@
 
 #include <lol/engine.h>
 #include <lol/pegtl>
+#include <cstring> // std::memchr
 #include <regex>
 
 namespace z8::pico8
@@ -30,100 +31,117 @@ using lol::msg;
 using lol::u8vec4;
 using lol::PixelFormat;
 
-static uint8_t const *compress_lut = nullptr;
-static char const *decompress_lut = "\n 0123456789abcdefghijklmnopqrstuvwxyz!#%(){}[]<>+=/*:;.,~_";
+static std::string decompress_new(uint8_t const *input);
+static std::string decompress_old(uint8_t const *input);
+static std::vector<uint8_t> compress_new(std::string const &input);
+static std::vector<uint8_t> compress_old(std::string const &input);
 
 std::string code::decompress(uint8_t const *input)
 {
-    std::string ret;
-
-    // Retrieve code, with optional decompression
     if (input[0] == '\0' && input[1] == 'p' &&
         input[2] == 'x' && input[3] == 'a')
+        return decompress_new(input);
+
+    if (input[0] == ':' && input[1] == 'c' &&
+        input[2] == ':' && input[3] == '\0')
+        return decompress_old(input);
+
+    auto end = (uint8_t const *)std::memchr(input, '\0', sizeof(pico8::memory::code));
+    auto len = end ? size_t(end - input) : sizeof(pico8::memory::code);
+    return std::string((char const *)input, len);
+}
+
+std::vector<uint8_t> code::compress(std::string const &input)
+{
+    return compress_old(input);
+}
+
+static uint8_t const *compress_lut = nullptr;
+static char const *decompress_lut = "\n 0123456789abcdefghijklmnopqrstuvwxyz!#%(){}[]<>+=/*:;.,~_";
+
+static std::string decompress_new(uint8_t const *input)
+{
+    // Move to front structure, reversed
+    std::vector<uint8_t> mtf(256);
+    for (int i = 0; i < 256; ++i)
+        mtf[i] = uint8_t(255 - i);
+
+    int length = input[4] * 256 + input[5];
+    int compressed = input[6] * 256 + input[7];
+    assert(compressed <= int(sizeof(pico8::memory::code)));
+
+    int offset = 8 * 8; // offset in bits
+    auto get_bits = [&](int count) -> uint32_t
     {
-        // Move to front structure, reversed
-        std::vector<uint8_t> mtf(256);
-        for (int i = 0; i < 256; ++i)
-            mtf[i] = uint8_t(255 - i);
+        uint32_t n = 0;
+        for (int i = 0; i < count; ++i, ++offset)
+            n |= ((input[offset >> 3] >> (offset & 0x7)) & 0x1) << i;
+        return n;
+    };
 
-        int length = input[4] * 256 + input[5];
-        int compressed = input[6] * 256 + input[7];
-        assert(compressed <= int(sizeof(pico8::memory::code)));
+    std::string ret;
 
-        int offset = 8 * 8; // offset in bits
-        auto get_bits = [&](int count) -> uint32_t
+    while (int(ret.size()) < length && offset < compressed * 8)
+    {
+        if (get_bits(1))
         {
-            uint32_t ret = 0;
-            for (int i = 0; i < count; ++i, ++offset)
-                ret |= ((input[offset >> 3] >> (offset & 0x7)) & 0x1) << i;
-            return ret;
-        };
-
-        while (int(ret.size()) < length && offset < compressed * 8)
+            int nbits = 4;
+            while (get_bits(1))
+                ++nbits;
+            int n = get_bits(nbits) + (1 << nbits) - 16;
+            uint8_t ch = mtf[255 - n];
+            mtf.erase(mtf.begin() + 255 - n);
+            if (!ch)
+                break;
+            mtf.push_back(ch);
+            ret.push_back(char(ch));
+        }
+        else
         {
-            if (get_bits(1))
-            {
-                int nbits = 4;
-                while (get_bits(1))
-                    ++nbits;
-                int n = get_bits(nbits) + (1 << nbits) - 16;
-                uint8_t ch = mtf[255 - n];
-                mtf.erase(mtf.begin() + 255 - n);
-                mtf.push_back(ch);
-                ret.push_back(char(ch));
-            }
-            else
-            {
-                int nbits = get_bits(1) ? get_bits(1) ? 5 : 10 : 15;
-                int offset = get_bits(nbits) + 1;
+            int nbits = get_bits(1) ? get_bits(1) ? 5 : 10 : 15;
+            int offset = get_bits(nbits) + 1;
 
-                int n, len = 3;
-                do
-                    len += (n = get_bits(3));
-                while (n == 7);
+            int n, len = 3;
+            do
+                len += (n = get_bits(3));
+            while (n == 7);
 
-                for (int i = 0; i < len; ++i)
-                    ret.push_back(ret[ret.size() - offset]);
-            }
+            for (int i = 0; i < len; ++i)
+                ret.push_back(ret[ret.size() - offset]);
         }
     }
-    else if (input[0] == ':' && input[1] == 'c' &&
-             input[2] == ':' && input[3] == '\0')
-    {
-        // Expected data length (including trailing zero)
-        int length = input[4] * 256 + input[5];
 
-        ret.resize(0);
-        for (int i = 8; i < (int)sizeof(pico8::memory::code) && (int)ret.length() < length; ++i)
+    return ret;
+}
+
+static std::string decompress_old(uint8_t const *input)
+{
+    std::string ret;
+
+    // Expected data length (including trailing zero)
+    int length = input[4] * 256 + input[5];
+
+    ret.resize(0);
+    for (int i = 8; i < (int)sizeof(pico8::memory::code) && (int)ret.length() < length; ++i)
+    {
+        if (input[i] >= 0x3c)
         {
-            if (input[i] >= 0x3c)
-            {
-                int a = (input[i] - 0x3c) * 16 + (input[i + 1] & 0xf);
-                int b = input[i + 1] / 16 + 2;
-                if ((int)ret.length() >= a)
-                    while (b--)
-                        ret += ret[ret.length() - a];
-                ++i;
-            }
-            else
-            {
-                ret += input[i] ? decompress_lut[input[i] - 1]
-                                        : input[++i];
-            }
+            int a = (input[i] - 0x3c) * 16 + (input[i + 1] & 0xf);
+            int b = input[i + 1] / 16 + 2;
+            if ((int)ret.length() >= a)
+                while (b--)
+                    ret += ret[ret.length() - a];
+            ++i;
         }
-
-        if (length != (int)ret.length())
-            msg::warn("expected %d code bytes, got %d\n", length, (int)ret.length());
+        else
+        {
+            ret += input[i] ? decompress_lut[input[i] - 1]
+                            : input[++i];
+        }
     }
-    else
-    {
-        int length = 0;
-        while (length < (int)sizeof(pico8::memory::code) && input[length] != '\0')
-            ++length;
 
-        ret.resize(length);
-        memcpy(&ret[0], &input, length);
-    }
+    if (length != (int)ret.length())
+        msg::warn("expected %d code bytes, got %d\n", length, (int)ret.length());
 
     // Remove possible trailing zeroes
     ret.resize(strlen(ret.c_str()));
@@ -131,7 +149,13 @@ std::string code::decompress(uint8_t const *input)
     return ret;
 }
 
-std::vector<uint8_t> code::compress(std::string const &input)
+static std::vector<uint8_t> compress_new(std::string const& input)
+{
+    std::vector<uint8_t> ret;
+    return ret;
+}
+
+static std::vector<uint8_t> compress_old(std::string const &input)
 {
     std::vector<uint8_t> ret;
 
